2a4b8bd986d8a399a208098f655c7a9dc6a00afa
Flavio <f>
2023-04-21 20:13:44 -0300

    Fix

--- ClubedaLeitura/Program.cs
+++ ClubedaLeitura/Program.cs
@@ -139,5 +139,51 @@ namespace ClubedaLeitura
                 }
             }
         }
+        private static void PopularRepositorios(
+            GuardaAmigo repositorioAmigo, GuardaCaixa repositorioCaixa,
+            GuardaRevista repositorioRevista, GuardaEmprestimo repositorioEmprestimo)
+        {
+            Pessoa amigo1 = new Pessoa("Gabriel Barbosa", "Jorge Sampaoli", "99999999", "Gávea");
+
+            Pessoa amigo2 = new Pessoa("Bruno Henrique", "Jorge Sampaoli", "99999999", "Gávea");
+
+            Pessoa amigo3 = new Pessoa("Pedro Quexada", "Jorge Sampaoli", "99999999", "Gávea");
+
+            repositorioAmigo.Inserir(amigo1);
+            repositorioAmigo.Inserir(amigo2);
+            repositorioAmigo.Inserir(amigo3);
+
+            Caixa caixa = new Caixa("Verde", "abc-123");
+
+            repositorioCaixa.Inserir(caixa);
+
+            Revista revista1 = new Revista("DC Comics", 10, 2010, caixa, "Batman Begins");
+
+            repositorioRevista.Inserir(revista1);
+
+            Revista revista2 = new Revista("DC Comics", 10, 2010, caixa, "As aventuras de superman");
+
+            repositorioRevista.Inserir(revista2);
+
+            Revista revista3 = new Revista("Mauricio de Sousa HQ's", 10, 2010, caixa, "Turma da Monica");
+
+            repositorioRevista.Inserir(revista3);
+
+            DateTime hoje = new DateTime(2023, 4, 19);
+
+            Emprestimo emprestimo1 = new Emprestimo(hoje, revista1, amigo1);
+
+            repositorioEmprestimo.Inserir(emprestimo1);
+
+            DateTime ontem = new DateTime(2023, 3, 18);
+            Emprestimo emprestimo2 = new Emprestimo(ontem, revista2, amigo2);
+
+            repositorioEmprestimo.Inserir(emprestimo2);
+
+            DateTime anteOntem = new DateTime(2022, 1, 17);
+            Emprestimo emprestimo3 = new Emprestimo(anteOntem, revista3, amigo3);
+
+            repositorioEmprestimo.Inserir(emprestimo3);
+        }
     }
 }
